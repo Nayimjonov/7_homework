7bd491c68b5bf8ca19c7d5dbd3d798b5b34a134e
Samandar <samandarnayimjonov@gmail.com>
2025-04-15 20:26:34 +0500

    meal food modelsfull done

--- meals/models.py
+++ meals/models.py
@@ -16,4 +16,14 @@ class Meal(models.Model):
     meal_type = models.CharField(choices=MEAL_TYPE, default=MEAL_TYPE[3][0])
     foods = models.ManyToManyField(Food, through='MealFood')
 
+    def __str__(self):
+        return f"{self.get_meal_type_display()} by {self.user.username} on {self.date}"
+
 class MealFood(models.Model):
+    meal = models.ForeignKey(Meal, on_delete=models.CASCADE, related_name='meal_foods')
+    food = models.ForeignKey(Food, on_delete=models.CASCADE, related_name='meal_foods')
+    quantity = models.DecimalField(max_digits=5, decimal_places=2),
+
+
+    def __str__(self):
+        return f"{self.quantity}g of {self.food.name} in {self.meal}"
