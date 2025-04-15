321efb60bacf2b5bd52f45c651294522ba467db0
Samandar <samandarnayimjonov@gmail.com>
2025-04-15 20:06:54 +0500

    done models

--- foods/models.py
+++ foods/models.py
@@ -5,4 +5,8 @@ class Food(models.Model):
     name = models.CharField(max_length=200)
     calories = models.PositiveIntegerField()
     protein = models.DecimalField(max_digits=5, decimal_places=2)
+    carbs = models.DecimalField(max_digits=5, decimal_places=2),
+    fats = models.DecimalField(max_digits=5, decimal_places=2),
 
+    def __str__(self):
+        return self.name
