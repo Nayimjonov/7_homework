128deccd1f0554acda7a6705870001f1e83a7f01
Samandar <samandarnayimjonov@gmail.com>
2025-04-15 20:01:12 +0500

    workout all model done

--- workouts/models.py
+++ workouts/models.py
@@ -1,4 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from exercise.models import Exercise
 
 
 class Workout(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='workouts')
+    date = models.DateField()
+    duration = models.DurationField()
+    exercises = models.ManyToManyField(Exercise, through='WorkoutExercise')
+
+    def __str__(self):
+        return f"Workout of {self.user.username} on {self.date}"
+
+
+class WorkoutExercise(models.Model):
+    workout = models.ForeignKey(Workout, on_delete=models.CASCADE)
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    sets = models.PositiveIntegerField()
+    reps = models.PositiveIntegerField()
+    weight = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.exercise.name} in workout {self.workout.id}"
