604a155543affcf837cb89ec61ddc12417a0722b
Samandar <samandarnayimjonov@gmail.com>
2025-04-15 20:05:32 +0500

    protein field added

--- foods/models.py
+++ foods/models.py
@@ -4,3 +4,5 @@ from django.db import models
 class Food(models.Model):
     name = models.CharField(max_length=200)
     calories = models.PositiveIntegerField()
+    protein = models.DecimalField(max_digits=5, decimal_places=2)
+
