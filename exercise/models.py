b50ff9fcf64b8d62905bde66a438cb36b9bdc9f4
Samandar <samandarnayimjonov@gmail.com>
2025-04-15 19:45:54 +0500

    category field qushildi + default 0 - indexni dinamik oladigan qlindi

--- exercise/models.py
+++ exercise/models.py
@@ -2,5 +2,16 @@ from django.db import models
 
 
 class Exercise(models.Model):
+    CHOICES_CATEGORY = [
+        ('cardio', 'Cardio'),
+        ('strength', 'Strength'),
+        ('flexibility', 'Flexibility'),
+        ('balance', 'Balance'),
+    ]
+
     name = models.CharField(max_length=200)
     description = models.TextField()
+    category = models.CharField(max_length=20, choices=CHOICES_CATEGORY, default=CHOICES_CATEGORY[0][0])
+
+    def __str__(self):
+        return self.name
