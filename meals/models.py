a7e60d22e2ab75596dfbcb0552dabb45c0d99b4c
Samandar <samandarnayimjonov@gmail.com>
2025-04-15 20:19:52 +0500

    meal model done

--- meals/models.py
+++ meals/models.py
@@ -1,5 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
+from foods.models import Food
 
 class Meal(models.Model):
+    MEAL_TYPE = [
+        ('breakfast', 'Breakfast'),
+        ('lunch', 'Lunch'),
+        ('dinner', 'Dinner'),
+        ('snack', 'Snack'),
+
+    ]
+
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='meals')
+    date = models.DateField()
+    meal_type = models.CharField(choices=MEAL_TYPE, default=MEAL_TYPE[3][0])
+    foods = models.ManyToManyField(Food, through='MealFood')
+
