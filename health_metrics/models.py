db44fa29fe2266029c3aa75ca7a8b2863cc1388f
Samandar <samandarnayimjonov@gmail.com>
2025-04-16 10:12:27 +0500

    health model done

--- health_metrics/models.py
+++ health_metrics/models.py
@@ -1,4 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class HealthMetrics(models.Model):
+    user  = models.ForeignKey(User, on_delete=models.CASCADE, related_name='health_metrics')
+    date = models.DateField()
+    weight = models.DecimalField(max_digits=5, decimal_places=2)
+    body_fat_percentage = models.DecimalField(max_length=5, decimal_places=2, null=True, blank=True),
+    blood_pressure_systolic = models.PositiveIntegerField(null=True, blank=True)
+    blood_pressure_diastolic = models.PositiveIntegerField(null=True, blank=True)
+    heart_rate = models.PositiveIntegerField(null=True, blank=True)
+
